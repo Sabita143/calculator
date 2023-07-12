27c2306db7803fcacdc54dbe952e7c9353dfdf30
Sabita143 <97331335+Sabita143@users.noreply.github.com>
2023-07-12 12:47:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let string="";
+let buttons= document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        if(e.target.innerHTML == 'C'){
+            string = ""
+            document.querySelector('input').value = string;
+        }
+        else{
+         console.log(e.target)
+         string= string+ e.target.innerHTML;
+         document.querySelector('input').value = string;
+        }
+    })
+})
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
+
